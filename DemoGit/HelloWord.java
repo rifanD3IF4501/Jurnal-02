01717b7c84ded665334102763c0877a1620989d3
rifanD3IF4501 <rifanmuhammadhidayat287.gmail.com>
2021-09-29 12:17:25 +0700

    HelloWord.java

---
+++ DemoGit/HelloWord.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama	: Rifan!");
+		System.out.println("NIM	: D3IF6706213084");
+		System.out.println("Kelas	: 45-01");
+	}
+}
+
+	public static void main(String[] args) {
+		System.out.println("Nama	: Rifan!");
+		System.out.println("NIM	: D3IF6706213084");
+		System.out.println("Kelas	: 45-01");
+	}
+}
